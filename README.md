8d0dad89d7f73362584f3475d030283fb9b4441b
nawalirfan <nawal.irfan1110@gmail.com>
2023-10-09 19:22:51 +0500

    assignment 01

---
+++ README.md
@@ -0,0 +1,10 @@
+# assignment_1
+
+Nawal Irfan 22805
+Application Development- Flutter
+Assignment 01
+
+Task: 
+- Display list of comments from api through future builder. 
+- On clicking a comment, details are shown on bottom modal.
+
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:assignment_1/list_comments.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Assignment 1',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ApiData(),
+    );
+  }
+}
